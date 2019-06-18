a44bc5a9f75f32c44872c6b2e7712fb3c7b36d71
Alex <therebeloper@gmail.com>
2019-06-18 09:33:52 +0300

    Initial Commit

---
+++ SwiftUIinUIKitwithDuckUI/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  SwiftUIinUIKitwithDuckUI
+//
+//  Created by Alex Nagy on 18/06/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
