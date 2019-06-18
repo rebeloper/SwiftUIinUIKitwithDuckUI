99b0b66c1d28ee7036be9fa26b6e106aee1747fc
Alex <therebeloper@gmail.com>
2019-06-18 09:55:12 +0300

    first commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '12.0'
+
+target 'SwiftUIinUIKitwithDuckUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftUIinUIKitwithDuckUI
+pod 'DuckUI'
+
+end
--- SwiftUIinUIKitwithDuckUI/RootViewController.swift
+++ SwiftUIinUIKitwithDuckUI/RootViewController.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2019 Alex Nagy. All rights reserved.
 //
 
-import UIKit
+import DuckUI
 
-class RootViewController: UIViewController {
+class RootViewController: D_ViewController {
+    
+    // MARK:- View
+    let helloDuckUILabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func setupViewsWith(safeArea: D_ViewSafeArea) {
+        super.setupViewsWith(safeArea: .all)
+        container.D_VStack(
+            helloDuckUILabel.text("Hello DuckUI")
+                .textAlignment(.center)
+        )
+    }
+    
+    override func observe() {
+        super.observe()
+    }
+    
+    override func bond() {
+        super.bond()
+    }
+    
+    override func fetchData() {
+        super.fetchData()
+    }
 
 }
 
